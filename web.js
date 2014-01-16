2e3969f933ddb3607f4f90b028483db118b3f6d1
francescotena <francesco.tena@gmail.com>
2014-01-16 18:31:57 -0500

    web.js using index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readfile(index.html, string));
 });
 
 var port = process.env.PORT || 5000;
