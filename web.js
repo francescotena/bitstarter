fa3568f281a246f8079af0d8dc9084df0657c4d6
francescotena <francesco.tena@gmail.com>
2014-01-16 19:30:35 -0500

    web.js finally correct

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
+var express = require('express');
 var fs = require('fs');
 
-var express = require('express');
+var data = fs.readFileSync("index.html","utf-8");
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readfile(index.html, string));
+  response.send(data);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
